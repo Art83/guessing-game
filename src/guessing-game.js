0dcb165efe6f842fa0978f58b5a814dfadde72af
Art83 <shvetsov1983@gmail.com>
2023-02-13 10:04:43 +1100

    init: implemented the guessing game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minimum = 0
+        this.maximum = 0
+        this.half = 0
+    }
 
     setRange(min, max) {
-
+        this.minimum = min
+        this.maximum = max
+        this.half = 0
     }
 
     guess() {
-
+        this.half = Math.round((this.minimum + this.maximum) / 2);
+        return this.half;
     }
 
     lower() {
-
+        this.maximum = this.half;
     }
 
     greater() {
-
+        this.minimum = this.half;
     }
 }
 
